0cff3306aa2e10f0ee446bde5e7681b18a535a90
Dáire McDermott <dairemcdermott@outlook.com>
2024-01-12 14:31:06 +0000

    adds test for win count after a loss

--- test/playertest.py
+++ test/playertest.py
@@ -33,6 +33,17 @@ class PlayerTest(unittest.TestCase):
 
         self.assertEqual(1, self.player.wins)
 
+    def test_player_win_count_after_loss(self):
+        self.ms.player = self.player
+        self.ms.code = [Colour.RED, Colour.BLUE, Colour.GREEN, Colour.YELLOW]
+
+        for i in range(10):
+            choice = [Colour.RED, Colour.BLUE, Colour.GREEN, Colour.BLACK]
+
+            self.ms.guess(choice)
+
+        self.assertEqual(0, self.player.wins)
+
 
 if __name__ == '__main__':
     unittest.main()
