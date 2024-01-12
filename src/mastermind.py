5cbb99120e3bb6ed70af70300d5acdd04dd844a4
Dáire McDermott <dairemcdermott@outlook.com>
2024-01-12 13:00:50 +0000

    changes guess() return type

--- src/mastermind.py
+++ src/mastermind.py
@@ -13,4 +13,12 @@ class Mastermind:
         self.code = self.random.choices(colours, k=4)
 
     def guess(self, guess):
-        return self.code == guess
+        result = [0, 0]
+
+        for i in range(len(self.code)):
+            if guess[i] == self.code[i]:
+                result[0] += 1
+            elif guess[i] in self.code:
+                result[1] += 1
+
+        return result
--- test/mastermindtest.py
+++ test/mastermindtest.py
@@ -4,8 +4,8 @@ from random import Random
 from colour import Colour
 from mastermind import Mastermind
 
-
 MAX_GUESSES = 10
+WINNING_RESULT = [4, 0]
 
 
 class MastermindTest(unittest.TestCase):
@@ -22,7 +22,7 @@ class MastermindTest(unittest.TestCase):
         choice = [Colour.RED, Colour.BLUE, Colour.GREEN, Colour.YELLOW]
 
         result = self.ms.guess(choice)
-        self.assertTrue(result)
+        self.assertEqual(WINNING_RESULT, result)
 
     def test_lose_with_max_guesses(self):
         self.ms.code = [Colour.RED, Colour.BLUE, Colour.GREEN, Colour.YELLOW]
@@ -31,7 +31,20 @@ class MastermindTest(unittest.TestCase):
             choice = self.random.choices([Colour.RED, Colour.BLUE, Colour.GREEN, Colour.BLACK], k=4)
 
             result = self.ms.guess(choice)
-            self.assertFalse(result)
+            self.assertNotEqual(WINNING_RESULT, result)
+
+    def test_win_with_max_guesses(self):
+        self.ms.code = [Colour.RED, Colour.BLUE, Colour.GREEN, Colour.YELLOW]
+
+        for i in range(MAX_GUESSES - 1):
+            choice = self.random.choices([Colour.RED, Colour.BLUE, Colour.GREEN, Colour.BLACK], k=4)
+
+            result = self.ms.guess(choice)
+            self.assertNotEqual(WINNING_RESULT, result)
+
+        choice = [Colour.RED, Colour.BLUE, Colour.GREEN, Colour.YELLOW]
+        result = self.ms.guess(choice)
+        self.assertEqual(WINNING_RESULT, result)
 
 
 class MastermindStub(Mastermind):
@@ -40,5 +53,5 @@ class MastermindStub(Mastermind):
         self.code = code
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
