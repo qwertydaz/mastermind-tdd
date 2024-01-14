b9e95b455adb1687ceff0cbbca7da2af9801ddbb
Dáire McDermott <dairemcdermott@outlook.com>
2024-01-14 23:23:00 +0000

    adds test for calculating score after winning with more than max guesses

--- src/util/scoreutil.py
+++ src/util/scoreutil.py
@@ -2,7 +2,8 @@ from constants.mastermindconstants import MastermindConstants
 
 
 def calculate_score(num_of_guesses):
-    if num_of_guesses < 1:
-        raise ValueError("number of guesses must be greater or equal to 1")
+    if not 1 <= num_of_guesses <= MastermindConstants.MAX_NUM_OF_GUESSES:
+        raise ValueError("number of guesses must be between 1 and " +
+                         str(MastermindConstants.MAX_NUM_OF_GUESSES) + " inclusive")
 
     return (MastermindConstants.MAX_NUM_OF_GUESSES - num_of_guesses + 1) * 100
--- test/util/scoreutiltest.py
+++ test/util/scoreutiltest.py
@@ -13,9 +13,13 @@ class ScoreUtilTest(unittest.TestCase):
         final_score = calculate_score(MastermindConstants.MAX_NUM_OF_GUESSES)
         self.assertEqual(100, final_score)
 
-    def test_score_with_negative_guesses(self):
+    def test_score_after_winning_with_negative_guesses(self):
         with self.assertRaises(ValueError):
             calculate_score(-1)
+
+    def test_score_after_winning_with_more_than_max_guesses(self):
+        with self.assertRaises(ValueError):
+            calculate_score(MastermindConstants.MAX_NUM_OF_GUESSES + 1)
 
 
 if __name__ == "__main__":
