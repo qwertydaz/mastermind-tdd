6432d9ca83e0497c6a2565ff7d10220132faa4c2
Dáire McDermott <dairemcdermott@outlook.com>
2024-01-14 23:00:12 +0000

    adds test for calculating score after winning with max num of guesses

--- src/util/scoreutil.py
+++ src/util/scoreutil.py
@@ -1,3 +1,5 @@
-class ScoreUtil:
-    def calculate_score(self):
-        return 1000
+from constants.mastermindconstants import MastermindConstants
+
+
+def calculate_score(num_of_guesses):
+    return (MastermindConstants.MAX_NUM_OF_GUESSES - num_of_guesses + 1) * 100
--- test/util/scoreutiltest.py
+++ test/util/scoreutiltest.py
@@ -1,13 +1,17 @@
 import unittest
 
-from util.scoreutil import ScoreUtil
+from constants.mastermindconstants import MastermindConstants
+from util.scoreutil import calculate_score
 
 
 class ScoreUtilTest(unittest.TestCase):
     def test_score_after_winning_in_1_guess(self):
-        num_of_guesses = 1
-        final_score = ScoreUtil.calculate_score(num_of_guesses)
+        final_score = calculate_score(1)
         self.assertEqual(1000, final_score)
+
+    def test_score_after_winning_in_max_guesses(self):
+        final_score = calculate_score(MastermindConstants.MAX_NUM_OF_GUESSES)
+        self.assertEqual(100, final_score)
 
 
 if __name__ == "__main__":
