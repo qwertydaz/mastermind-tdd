1d5869b80c015792ff4ba005a428205230d9e2c2
Dáire McDermott <dairemcdermott@outlook.com>
2024-01-15 12:06:53 +0000

    adds valid/invalid tests for end result

--- src/entity/result/endresult.py
+++ src/entity/result/endresult.py
@@ -1,2 +1,11 @@
+from src.util.intutil import is_num_of_guesses_valid
+
+
 class EndResult:
-    pass
+    def __init__(self, num_of_guesses):
+        if is_num_of_guesses_valid(num_of_guesses):
+            self._num_of_guesses = num_of_guesses
+
+    @property
+    def num_of_guesses(self):
+        return self._num_of_guesses
--- test/entity/result/test_endresult.py
+++ test/entity/result/test_endresult.py
@@ -1,9 +1,27 @@
 import unittest
 
+from constants.mastermindconstants import MastermindConstants
+from src.entity.result.endresult import EndResult
+
 
 class EndResultTest(unittest.TestCase):
-    def test_something(self):
-        self.fail()
+    def test_end_result_with_invalid_range_of_guesses(self):
+        with self.assertRaises(ValueError):
+            EndResult(-1)
+        with self.assertRaises(ValueError):
+            EndResult(0)
+        with self.assertRaises(ValueError):
+            EndResult(MastermindConstants.MAX_NUM_OF_GUESSES + 1)
+
+    def test_end_result_with_invalid_datatypes_for_guesses(self):
+        with self.assertRaises(TypeError):
+            EndResult("1")
+        with self.assertRaises(TypeError):
+            EndResult(1.0)
+        with self.assertRaises(TypeError):
+            EndResult(None)
+        with self.assertRaises(TypeError):
+            EndResult(True)
 
 
 if __name__ == "__main__":
