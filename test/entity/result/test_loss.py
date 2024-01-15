5af429675def2efab44d51fb659751986f5bcf59
Dáire McDermott <dairemcdermott@outlook.com>
2024-01-15 12:20:41 +0000

    adds test for num of guesses for loss

--- test/entity/result/test_loss.py
+++ test/entity/result/test_loss.py
@@ -1,9 +1,13 @@
 import unittest
 
+from constants.mastermindconstants import MastermindConstants
+from src.entity.result.loss import Loss
+
 
 class LossTest(unittest.TestCase):
-    def test_something(self):
-        self.fail()
+    def test_valid_loss(self):
+        loss = Loss()
+        self.assertEqual(MastermindConstants.MAX_NUM_OF_GUESSES, loss.num_of_guesses)
 
 
 if __name__ == "__main__":
